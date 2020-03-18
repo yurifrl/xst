14c4984f4e007bc35b24a1a9208e00d8f447f836
yuri <yurifl03@gmail.com>
2020-03-18 20:20:00 -0300

    feature: add fallback font

--- src/config.h
+++ src/config.h
@@ -7,6 +7,9 @@ static unsigned int rows = 24;
 
 /* ref: http://freedesktop.org/software/fontconfig/fontconfig-user.html */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true;";
+static char *font2[] = {
+                        "Symbola:pixelsize=12:antialias=true:autohint=true",
+};
 
 /* exec precedence: -e arg, utmp option, SHELL env var, /etc/passwd shell */
 static char *shell = "\0";
